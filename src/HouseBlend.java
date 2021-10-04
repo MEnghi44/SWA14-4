1ab617357c26042a75bea07cf1bbc622e5dbe611
MEnghi44 <69116659+MEnghi44@users.noreply.github.com>
2021-10-04 16:37:55 +0700

    Initial commit

---
+++ src/HouseBlend.java
@@ -0,0 +1,9 @@
+
+public class HouseBlend extends Beverage {
+	public HouseBlend() {
+		description = "House Blenf Coffee";
+	}
+	public double cost() {
+		return .89;
+	}
+}
---
+++ src/StarbuzzCoffee.java
@@ -0,0 +1,7 @@
+
+public class StarbuzzCoffee {
+	public static void main(String args[]) {
+		Beverage beverage = new Espresso();
+		System.out.println(be)
+	}
+}
